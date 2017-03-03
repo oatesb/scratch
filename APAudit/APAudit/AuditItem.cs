17b02745b9570563b1ce3e130005015dcd78e2c3
Ben Oates <beoates@microsoft.com>
2017-03-03 14:03:02 -0800

    Add APAudit to scratch

---
+++ APAudit/APAudit/AuditItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APAudit
+{
+    public class AuditItem
+    {
+        public string Section { get; set; }
+        List<AuditResults> Audits;
+    }
+}
---
+++ APAudit/APAudit/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IniParser;
+using IniParser.Model;
+
+namespace APAudit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            var parser = new FileIniDataParser();
+
+            parser.Parser.Configuration.AssigmentSpacer = "";
+            
+
+            List<string> folderFilter = new List<string>();
+            folderFilter.Add("bn2");
+            folderFilter.Add("goodsfsdf");
+            folderFilter.Add("global");
+
+            List<string> fileFilter = new List<string>();
+            fileFilter.Add("bn2");
+            fileFilter.Add("Environment.ini");
+
+
+            var files = Utils.GetFilteredFiles(".", SearchOption.AllDirectories, folderFilter, fileFilter);
+
+            foreach (var item in files)
+            {
+                IniData data = parser.ReadFile(item.FullName);
+
+                data["General"].AddKey("Ben", "Oates");
+
+                //data.Sections.AddSection("FoooBaaar");
+                //data["FoooBaaar"].AddKey("Hello", "GoodBye");
+
+
+                string newFile = Path.Combine(item.DirectoryName + Path.DirectorySeparatorChar + Path.GetFileNameWithoutExtension(item.Name) + "_temp" + item.Extension);
+                parser.WriteFile(newFile, data);
+
+            }
+           
+
+            Console.Read();
+        }
+    }
+
+
+    public static class Utils
+    {
+        public static List<FileInfo> GetFilteredFiles(string rootFolder, SearchOption resuriveFlag, List<string> folderNameContains, List<string> fileNameEquals )
+        {
+            var files = new DirectoryInfo(rootFolder).GetFiles("*", resuriveFlag);
+
+            List<FileInfo> returnme = new List<FileInfo>();
+            foreach (var item in files)
+            {
+                Console.WriteLine("Process: {0}", item.FullName);
+                if (folderNameContains != null && folderNameContains.Any(x=> item.DirectoryName.Contains(x)))
+                {
+                    Console.WriteLine("\t{0} Passed the folder filter.  Seeing if it now passed the file filters", item.FullName);
+                    if (fileNameEquals != null && fileNameEquals.Any(x=> item.Name.Equals(x, StringComparison.CurrentCultureIgnoreCase)))
+                    {
+                        Console.WriteLine("\t\tAdd: {0}", item.FullName);
+                        returnme.Add(item);
+                    }
+                    
+                }
+                
+
+            }
+
+            return returnme;
+        }
+    }
+}
